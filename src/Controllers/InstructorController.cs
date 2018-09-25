404a0ea2c8ad038e31db4b92a97975d24d8c2bb8
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-09-25 13:49:57 -0500

    attempting to add instructor controller

---
+++ src/Controllers/InstructorController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Workforce.Models;
+using Workforce.Models.ViewModels;
+using System.Data.SqlClient;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Workforce.Controllers
+{
+	public class InstructorController : Controller
+	{
+
+		private readonly IConfiguration _config;
+
+		public InstructorController(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public IDbConnection Connection
+		{
+			get
+			{
+				return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+			}
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			string sql = @"
+			SELECT
+				i.Id,
+				i.FirstName,
+				i.LastName,
+				i.SlackHandle,
+				i.Specialty,
+				c.Id,
+				c.Name
+			FROM Instructor i
+			JOIN Cohort c ON i.CohortId = c.Id
+			";
+
+			using (IDbConnection conn = Connection)
+			{
+				Dictionary<int, Instructor> instructors = new Dictionary<int, Instructor>();
+
+				var instructorQuerySet = await conn.QueryAsync<Instructor, Cohort, Instructor> (
+					sql, 
+					)
+			}
+		}
+	}
+}
